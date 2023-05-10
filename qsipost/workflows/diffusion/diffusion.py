74975c6bc4d0d50436cc9540949a97a1023ea196
GalKepler <galkepler@gmail.com>
2023-05-10 13:56:49 +0300

    added tractography outputnode

--- qsipost/workflows/diffusion/diffusion.py
+++ qsipost/workflows/diffusion/diffusion.py
@@ -175,7 +175,7 @@ def init_diffusion_wf(
                         connectome_wf,
                         [
                             (
-                                "outputnode.sift_tracts",
+                                "mrtrix_tractography_wf.outputnode.sift_tracts",
                                 "inputnode.in_tracts",
                             ),
                         ],
@@ -190,7 +190,7 @@ def init_diffusion_wf(
                         connectome_wf,
                         [
                             (
-                                "outputnode.unfiltered_tracts",
+                                "mrtrix_tractography_wf.outputnode.unfiltered_tracts",
                                 "inputnode.in_tracts",
                             ),
                         ],
