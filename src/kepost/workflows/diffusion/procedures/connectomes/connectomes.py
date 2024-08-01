a034fd35318b19930c14dd842962c137f8e8f362
GalKepler <galkepler@gmail.com>
2024-08-01 18:22:44 +0300

    lowering complexity

--- src/kepost/workflows/diffusion/procedures/connectomes/connectomes.py
+++ src/kepost/workflows/diffusion/procedures/connectomes/connectomes.py
@@ -35,16 +35,18 @@ def init_connectome_wf(name: str = "connectome_wf") -> pe.Workflow:
         ),
         name="inputnode",
     )
+
     fields = []
-    for in_tracts in ["tracts_sifted", "tracts_unsifted"]:
-        for combination in COMBINATIONS:
-            scale, metric = combination["scale"], combination["stat_edge"]
-            fields.append(
-                f"connectome_scale-{scale}_metric-{metric}_in-tracts-{in_tracts}"
-            )
-            fields.append(
-                f"assignments_scale-{scale}_metric-{metric}_in-tracts-{in_tracts}"
-            )
+    for combination in COMBINATIONS:
+        in_tracts, scale, metric = (
+            combination["in_tracts"],
+            combination["scale"],
+            combination["stat_edge"],
+        )
+        fields.append(f"connectome_scale-{scale}_metric-{metric}_in-tracts-{in_tracts}")
+        fields.append(
+            f"assignments_scale-{scale}_metric-{metric}_in-tracts-{in_tracts}"
+        )
     outputnode = pe.Node(
         niu.IdentityInterface(fields=fields + ["atlas_name"]),
         name="outputnode",
@@ -88,128 +90,131 @@ def init_connectome_wf(name: str = "connectome_wf") -> pe.Workflow:
             ),
         ]
     )
-    for in_tracts in ["tracts_sifted", "tracts_unsifted"]:
-        for combination in COMBINATIONS:
-            scale, metric = combination["scale"], combination["stat_edge"]
-            connectome = pe.Node(
-                mrt.BuildConnectome(
-                    stat_edge=metric,
-                    out_assignments="assignments.csv",
+    for combination in COMBINATIONS:
+        in_tracts, scale, metric = (
+            combination["in_tracts"],
+            combination["scale"],
+            combination["stat_edge"],
+        )
+        connectome = pe.Node(
+            mrt.BuildConnectome(
+                stat_edge=metric,
+                out_assignments="assignments.csv",
+            ),
+            name=f"connectome_scale-{scale}_metric-{metric}_in-tracts-{in_tracts}",
+        )
+        if scale is not None:
+            connectome.inputs.scale = scale
+        ds_connectome = pe.Node(
+            DerivativesDataSink(
+                suffix="connectome",
+                scale=scale if scale is not None else "raw",
+                measure=metric,
+                subtype="connectomes",
+                extension=".csv",
+                weight="SIFT2" if "unsifted" in in_tracts else "SIFT",
+            ),
+            name=f"ds_connectome_scale-{scale}_metric-{metric}_in-tracts-{in_tracts}",
+            run_without_submitting=True,
+        )
+        ds_assignments = pe.Node(
+            DerivativesDataSink(
+                suffix="assignments",
+                scale=scale,
+                measure=metric,
+                subtype="connectomes",
+                extension=".csv",
+                weight="SIFT2" if "unsifted" in in_tracts else "SIFT",
+            ),
+            name=f"ds_assignments_scale-{scale}_metric-{metric}_in-tracts-{in_tracts}",
+            run_without_submitting=True,
+        )
+        workflow.connect(
+            [
+                (
+                    inputnode,
+                    connectome,
+                    [
+                        (in_tracts, "in_tracts"),
+                        ("atlas_nifti", "in_nodes"),
+                    ],
                 ),
-                name=f"connectome_scale-{scale}_metric-{metric}_in-tracts-{in_tracts}",
-            )
-            if scale is not None:
-                connectome.inputs.scale = scale
-            ds_connectome = pe.Node(
-                DerivativesDataSink(
-                    suffix="connectome",
-                    scale=scale if scale is not None else "raw",
-                    measure=metric,
-                    subtype="connectomes",
-                    extension=".csv",
-                    weight="SIFT2" if "unsifted" in in_tracts else "SIFT",
+                (
+                    connectome,
+                    ds_connectome,
+                    [("out_connectome", "in_file")],
                 ),
-                name=f"ds_connectome_scale-{scale}_metric-{metric}_in-tracts-{in_tracts}",
-                run_without_submitting=True,
-            )
-            ds_assignments = pe.Node(
-                DerivativesDataSink(
-                    suffix="assignments",
-                    scale=scale,
-                    measure=metric,
-                    subtype="connectomes",
-                    extension=".csv",
-                    weight="SIFT2" if "unsifted" in in_tracts else "SIFT",
+                (
+                    inputnode,
+                    ds_connectome,
+                    [
+                        ("base_directory", "base_directory"),
+                        ("tracts_unsifted", "source_file"),
+                    ],
                 ),
-                name=f"ds_assignments_scale-{scale}_metric-{metric}_in-tracts-{in_tracts}",
-                run_without_submitting=True,
-            )
+                (
+                    get_atlas_name_node,
+                    ds_connectome,
+                    [
+                        ("atlas_name", "atlas"),
+                    ],
+                ),
+                (
+                    get_reconstruction_software,
+                    ds_connectome,
+                    [
+                        ("reconstruction_software", "reconstruction_software"),
+                    ],
+                ),
+                (
+                    connectome,
+                    ds_assignments,
+                    [("out_assignments", "in_file")],
+                ),
+                (
+                    inputnode,
+                    ds_assignments,
+                    [
+                        ("base_directory", "base_directory"),
+                        ("tracts_unsifted", "source_file"),
+                    ],
+                ),
+                (
+                    get_atlas_name_node,
+                    ds_assignments,
+                    [
+                        ("atlas_name", "atlas"),
+                    ],
+                ),
+                (
+                    get_reconstruction_software,
+                    ds_assignments,
+                    [
+                        ("reconstruction_software", "reconstruction_software"),
+                    ],
+                ),
+                (
+                    connectome,
+                    outputnode,
+                    [
+                        (
+                            "out_connectome",
+                            f"out_scale-{scale}_metric-{metric}_in-tracts-{in_tracts}",
+                        ),
+                    ],
+                ),
+            ]
+        )
+        if "unsifted" in in_tracts:
             workflow.connect(
                 [
                     (
                         inputnode,
                         connectome,
                         [
-                            (in_tracts, "in_tracts"),
-                            ("atlas_nifti", "in_nodes"),
-                        ],
-                    ),
-                    (
-                        connectome,
-                        ds_connectome,
-                        [("out_connectome", "in_file")],
-                    ),
-                    (
-                        inputnode,
-                        ds_connectome,
-                        [
-                            ("base_directory", "base_directory"),
-                            ("tracts_unsifted", "source_file"),
-                        ],
-                    ),
-                    (
-                        get_atlas_name_node,
-                        ds_connectome,
-                        [
-                            ("atlas_name", "atlas"),
-                        ],
-                    ),
-                    (
-                        get_reconstruction_software,
-                        ds_connectome,
-                        [
-                            ("reconstruction_software", "reconstruction_software"),
-                        ],
-                    ),
-                    (
-                        connectome,
-                        ds_assignments,
-                        [("out_assignments", "in_file")],
-                    ),
-                    (
-                        inputnode,
-                        ds_assignments,
-                        [
-                            ("base_directory", "base_directory"),
-                            ("tracts_unsifted", "source_file"),
-                        ],
-                    ),
-                    (
-                        get_atlas_name_node,
-                        ds_assignments,
-                        [
-                            ("atlas_name", "atlas"),
-                        ],
-                    ),
-                    (
-                        get_reconstruction_software,
-                        ds_assignments,
-                        [
-                            ("reconstruction_software", "reconstruction_software"),
-                        ],
-                    ),
-                    (
-                        connectome,
-                        outputnode,
-                        [
-                            (
-                                "out_connectome",
-                                f"out_scale-{scale}_metric-{metric}_in-tracts-{in_tracts}",
-                            ),
+                            ("tck_weights", "tck_weights_in"),
                         ],
                     ),
                 ]
             )
-            if "unsifted" in in_tracts:
-                workflow.connect(
-                    [
-                        (
-                            inputnode,
-                            connectome,
-                            [
-                                ("tck_weights", "tck_weights_in"),
-                            ],
-                        ),
-                    ]
-                )
     return workflow
--- src/kepost/workflows/diffusion/procedures/connectomes/utils.py
+++ src/kepost/workflows/diffusion/procedures/connectomes/utils.py
@@ -4,6 +4,10 @@ Utility functions for the connectome module.
 
 SCALES = ["length", "invlength", "invnodevol", None]
 STATS = ["sum", "mean", "min", "max"]
+IN_TRACTS = ["tracts_sifted", "tracts_unsifted"]
 COMBINATIONS = [
-    {"scale": scale, "stat_edge": metric} for scale in SCALES for metric in STATS
+    {"in_tracts": in_tracts, "scale": scale, "stat_edge": metric}
+    for in_tracts in IN_TRACTS
+    for scale in SCALES
+    for metric in STATS
 ]
